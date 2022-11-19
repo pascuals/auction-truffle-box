8b624da4f37fb2f46b984fee06b75406b2404c17
napascual <napascual@gmail.com>
2022-11-19 10:21:17 +0100

    fix(scripts): upgrade bid script code, update artifact import, correct bid transaction

--- scripts/bid.js
+++ scripts/bid.js
@@ -1,22 +1,28 @@
 /*
   Try `truffle exec scripts/bid.js`, you should `truffle migrate` first.
 
-  Learn more about Truffle external scripts: 
+  Learn more about Truffle external scripts:
   https://trufflesuite.com/docs/truffle/getting-started/writing-external-scripts
 */
 
-const SimpleStorage = artifacts.require("Auction");
+const SimpleStorage = artifacts.require("./Auction.sol");
 
 module.exports = async function (callback) {
     const deployed = await SimpleStorage.deployed();
 
-    const highestPrice = (await deployed.getHighestPrice()).toNumber();
+    const highestPrice = +(await deployed.getHighestPrice()).toString();
     console.log(`Current highest price value: ${highestPrice}`);
 
-    const { tx } = await deployed.bid().send({ value: highestPrice });
+    const basePrice = +(await deployed.getBasePrice()).toString();
+    console.log(`Current base price value: ${basePrice}`);
+
+    const nextBid = Math.max(highestPrice, basePrice) + 1000;
+    console.log(`Next bid: ${nextBid}`)
+
+    const { tx } = await deployed.bid.sendTransaction({ value: nextBid });
     console.log(`Confirmed transaction ${tx}`);
 
-    const updatedPrice = (await deployed.getHighestPrice()).toNumber();
+    const updatedPrice = +(await deployed.getHighestPrice()).toString();
     console.log(`Updated SimpleStorage value: ${updatedPrice}`);
 
     callback();
